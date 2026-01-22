eaaa5b47b1283046b6032ab9aaf125a959d2d5b7
zairi ghazali <mohdzairighazali@yahoo.com>
2026-01-22 15:00:02 +0800

    css fixed for mobile responsive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -98,7 +98,7 @@ export default function HomePage() {
         <div
           style={{
             position: "absolute",
-            top: "70%",
+            top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
             zIndex: 10,
